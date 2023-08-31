9b637f2d8222666d0472ee0dd6350918b9c69b2d
vyastest <142485679+vyastest@users.noreply.github.com>
2023-08-31 21:57:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ openai.api_key = st.secrets["my_api_key"]
 #generated_text = response.choices[0].text
 #st.write(generated_text)
 
-
 from langchain.llms import OpenAI
+#from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
 from langchain.agents.agent_types import AgentType
 from langchain.agents import create_csv_agent
