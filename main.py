e7d2bf49b071c18809b7dbb53ee9e2b040e6c133
vyastest <142485679+vyastest@users.noreply.github.com>
2023-08-31 22:20:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,11 @@ st.sidebar.title("ChatGPT-")
 
 openai.api_key = st.secrets["my_api_key"]
 
+data=pd.read_csv("teststream.csv")
+question="which striker has the most runs off bat?"
+prompt = f"Dataset:\n{data}\nQuestion: {question}\nInsights:"
 
-prompt = "india are the best team in cricket and have played strong games in past 10 years winning in all countries home and away..Please summarise the sentence in 8 words"
+#prompt = "india are the best team in cricket and have played strong games in past 10 years winning in all countries home and away..Please summarise the sentence in 8 words"
 model = "text-davinci-003"
 response = openai.Completion.create(engine=model, prompt=prompt, max_tokens=50)
 
